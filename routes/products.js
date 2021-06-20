bcf2f20d01392a1d9d604b07444795b11e77f852
Vitor Costa Jose <vitor.costa@prontmed.com>
2021-06-20 16:47:36 -0300

    adicionando hasSession para validar quando temos uma sessão ativa no servidor

--- routes/products.js
+++ routes/products.js
@@ -7,10 +7,15 @@ const authMiddleware = require("../middlewares/auth");
 // GET - /products
 router.get("/", authMiddleware.validateRole("manager"), function (req, res) {
   const productsData = productsModel.getProducts();
+  let hasSession = false;
+  if (req.session !== undefined && req.session.user !== undefined) {
+    hasSession = true;
+  }
 
   res.render("products", {
     title: "Pagina de produtos",
     productsData: productsData,
+    hasSession,
   });
 });
 
